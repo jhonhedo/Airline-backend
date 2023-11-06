eb0faf530dd128d469f18e66ae4ff4c9305828fb
Nanda Kishore <tnandakishore723@gmail.com>
2023-11-06 11:55:30 +0530

    cross

--- Airline-Reservation/src/main/java/com/abm/controller/AirlineController.java
+++ Airline-Reservation/src/main/java/com/abm/controller/AirlineController.java
@@ -2,15 +2,17 @@ package com.abm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.abm.repository.AirlineRepository;
 
 @RestController
+@CrossOrigin
 public class AirlineController {
-	
+
 	@Autowired
-	 private AirlineRepository airlineRepository;
+	private AirlineRepository airlineRepository;
 	//start writing the methods regarding to airline
 
 }
